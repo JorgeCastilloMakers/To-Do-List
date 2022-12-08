b35e29638fdd6074a2bd54f0729b3701a591fb71
Jorge Castillo <103472285+JorgeCastilloMakers@users.noreply.github.com>
2022-12-08 09:35:19 -0300

    update

--- src/components/taskForm/TaskForm.jsx
+++ src/components/taskForm/TaskForm.jsx
@@ -17,7 +17,7 @@ function TaskForm() {
                 alert("Please, complete all the fields");
                 return
             }
-
+            {console.log(tasks.find(task => task.title === title)}
             {(tasks.find(task => task.title === title)) === true ? alert("The task already exist") :  createTask({
             title,
             description
